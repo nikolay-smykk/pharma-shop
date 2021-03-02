6b68ddc137e2c97c7971fb4b87ab27288f8c21b3
Nikolay Smyk <41842687+nikolay-smykk@users.noreply.github.com>
2021-03-02 16:59:22 +0300

    mock and product

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,25 +1,14 @@
-import logo from './logo.svg';
-import './App.css';
+import React from 'react';
+import { motion } from 'framer-motion';
 
-function App() {
+import Products from './components/Poducts/Products';
+
+const App = () => {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <motion.div animate={{ scale: 0.5 }} transition={{ duration: 2 }}>
+      <Products />
+    </motion.div>
   );
-}
+};
 
 export default App;
---
+++ frontend/src/components/Poducts/Products.jsx
@@ -0,0 +1,24 @@
+import React from 'react'
+import {Grid} from '@material-ui/core'
+
+import Product from '../Product/Product'
+
+import {ProductList} from '../../mocs/product'
+
+
+const Products = () => {
+    return (
+        <main>
+            <Grid container justify="center" spacing={4}>
+                { ProductList.map(product =>
+                <Grid item key={product.id} xs={12} sm={6} md={4} lg={3}>
+                   
+                    <Product product={product}/>
+                </Grid>
+            )}
+            </Grid>
+        </main>
+    )
+}
+
+export default Products
