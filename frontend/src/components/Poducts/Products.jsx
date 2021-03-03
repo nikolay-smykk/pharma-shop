5f8dca4b3fbb4ab6ed7a30e11e9787aac310ce87
Nikolay Smyk <41842687+nikolay-smykk@users.noreply.github.com>
2021-03-03 17:03:29 +0300

    styles products

--- frontend/src/components/Poducts/Products.jsx
+++ frontend/src/components/Poducts/Products.jsx
@@ -2,13 +2,20 @@ import React from 'react'
 import {Grid} from '@material-ui/core'
 
 import Product from '../Product/Product'
+import useStyles from './styles'
 
 import {ProductList} from '../../mocs/product'
 
 
 const Products = () => {
+
+    const classes = useStyles();
+
     return (
-        <main>
+        <main className={classes.content}>
+            <div className={classes.toolbar}>
+
+            
             <Grid container justify="center" spacing={4}>
                 { ProductList.map(product =>
                 <Grid item key={product.id} xs={12} sm={6} md={4} lg={3}>
@@ -17,6 +24,7 @@ const Products = () => {
                 </Grid>
             )}
             </Grid>
+            </div>
         </main>
     )
 }
--- frontend/src/components/Product/styles.js
+++ frontend/src/components/Product/styles.js
@@ -2,12 +2,11 @@ import { makeStyles } from '@material-ui/core/styles';
 
 export default makeStyles(() => ({
   root: {
-    // maxWidth: 345, original width style
     maxWidth: '100%',
   },
   media: {
     height: 0,
-    paddingTop: '56.25%', // 16:9
+    paddingTop: '56.25%',
   },
   cardActions: {
     display: 'flex',
